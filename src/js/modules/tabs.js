8205543883c13c236ce3647f8f1403102f60d2dd
Dmitry Ageev <dimichageev@icloud.com>
2020-09-08 18:31:25 +0300

    004 14min

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,7 +1,34 @@
+'use strict';
+
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
     const header = document.querySelector(headerSelector),
         tab = document.querySelectorAll(tabSelector),
-        countent
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.remove(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains(tabSelector.replace(/\./, ""))) {
+            
+        }
+    })
 };
 
 export default tabs;
