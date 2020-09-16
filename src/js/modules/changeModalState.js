056a74239f4dd2d6ef3ac1b96e2028b7c7485611
Dmitry Ageev <dimichageev@icloud.com>
2020-09-16 17:34:27 +0300

    007 23min

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,5 +1,33 @@
-const changeModalState = (state) => {
+import checkNumInputs from './checkNumInputs';
 
+const changeModalState = (state) => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+        windowWidth = document.querySelectorAll('#width'),
+        windowHeight = document.querySelectorAll('#height'),
+        windowType = document.querySelectorAll('#view_type'),
+        windowProfile = document.querySelectorAll('.checkbox');
+
+    checkNumInputs('#width');
+    checkNumInputs('#height');
+
+    function bindActionToElem(event, elem, prop) {
+        elem.forEach((item, i) => {
+            item.addEventListener(event, () => {
+                if (elem.lenght > 1) {
+                    state[prop] = i;
+                } else {
+                    state[prop] = item.value;
+                }
+                console.log(state);
+            });
+        });
+    }
+
+    bindActionToElem('click', windowForm, 'form');
+    bindActionToElem('input', windowHeight, 'height');
+    bindActionToElem('input', windowWidth, 'width');
+    bindActionToElem('change', windowType, 'type');
+    bindActionToElem('change', windowProfile, 'profile');
 };
 
 export default changeModalState;
