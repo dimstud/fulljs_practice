e5cb4e162e870af60a9cb67994c32e6f7f3688d8
Dmitry Ageev <dimichageev@gmail.com>
2020-10-08 16:59:38 +0300

    016 end

--- Picture/src/js/modules/checkTextInputs.js
+++ Picture/src/js/modules/checkTextInputs.js
@@ -1,8 +1,8 @@
-const checkTextInputs = (selector) => {
+﻿const checkTextInputs = (selector) => {
     const txtInputs = document.querySelectorAll(selector);
 
     txtInputs.forEach(input => {
-        input.addEventListener('keypress', function(e) {
+        input.addEventListener('keypress', function (e) {
             if (e.key.match(/[^а-яё 0-9]/ig)) {
                 e.preventDefault();
             }
