b4c977f972dc86fea5c1e9a32d54fc3763399206
Dmitry Ageev <dimichageev@gmail.com>
2020-10-09 11:31:32 +0300

    017 14min

--- Picture/src/js/modules/showMoreStyles.js
+++ Picture/src/js/modules/showMoreStyles.js
@@ -21,6 +21,27 @@ const showMoreStyles = (trigger, style) => {
         getResource('http://localhost:3000/styles')
             .then(res => console.log(res));
     });
+
+    function createCards(response) {
+        response.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML = `
+
+                `
+        });
+    };
+
+    //<div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
+    //    <div class=styles-block>
+    //        <img src=assets /img/styles-8.jpg alt>
+				//		<h4>Шарж</h4>
+    //        <a href="#">Подробнее</a>
+    //    </div>
+    //</div>
+
 };
 
 export default showMoreStyles;
