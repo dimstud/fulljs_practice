1f69c2791186bcbf9811c01b5c683ae60ac6168c
Dmitry Ageev <dimichageev@gmail.com>
2020-10-10 18:02:09 +0300

    019 5min

---
+++ Picture/src/js/modules/filter.js
@@ -0,0 +1,25 @@
+﻿const filter = () => {
+	const menu = document.querySelector('.portfolio-menu'),
+		items = menu.querySelectorAll('li'),
+		btnAll = menu.querySelector('.all'),
+		btnLovers = menu.querySelector('.lovers'),
+		btnChef = menu.querySelector('.chef'),
+		btnGirl = menu.querySelector('.girl'),
+		btnGuy = menu.querySelector('.guy'),
+		btnGrandmother = menu.querySelector('.grandmother'),
+		btngranddad = menu.querySelector('.granddad'),
+		wrapper = document.querySelector('portfolio-wrapper'),
+		markAll = wrapper.querySelector('.all'),
+		markGirl = wrapper.querySelector('.girl'),
+		markLovers = wrapper.querySelector('.lovers'),
+		markChef = wrapper.querySelectorAll('.chef'),
+		markCuy = wrapper.querySelectorAll('.guy'),
+		no = document.querySelector('.portfoli0-no');
+
+	const typeFilter = (markType) => {
+
+	};
+		
+};
+
+export default filter;
