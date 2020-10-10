809592d776c30e2de8d5185bfd4bb4241269edc0
Dmitry Ageev <dimichageev@gmail.com>
2020-10-10 12:09:16 +0300

    018 12min

---
+++ Picture/src/js/modules/calc.js
@@ -0,0 +1,17 @@
+﻿const calc = (size, material, options, promocode, result) => {
+    const sizeBlock = document.querySelector(size),
+        materialBlock = document.querySelector(material),
+        optionsBlock = document.querySelector(options),
+        promocodeBlock = document.querySelector(promocode),
+        resultBlock = document.querySelector(result);
+
+    let sum = 0;
+
+    const calcFunc = () => {
+        sum = Math.round((+sizeBlock.value) * (+materialBlock.value) * (+optionsBlock.value));
+
+        if ()
+    };
+};
+
+export default calc;
