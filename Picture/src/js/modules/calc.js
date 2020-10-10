835ceaf50f6409c1b84ab61f136e40b4910ce2ee
Dmitry Ageev <dimichageev@gmail.com>
2020-10-10 17:13:28 +0300

    018 end

--- Picture/src/js/modules/calc.js
+++ Picture/src/js/modules/calc.js
@@ -8,10 +8,21 @@
     let sum = 0;
 
     const calcFunc = () => {
-        sum = Math.round((+sizeBlock.value) * (+materialBlock.value) * (+optionsBlock.value));
+        sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
 
-        if ()
-    };
+        if (sizeBlock.value == '' || materialBlock.value == '') {
+            resultBlock.textContent = "Пожалуйста, выберите размер и материал каритны";
+        } else if (promocodeBlock.value === 'IWANTPOPART') {
+            resultBlock.textContent = Math.round(sum * 0.7);
+        } else {
+            resultBlock.textContent = sum;
+        }
+    };      
+
+    sizeBlock.addEventListener('change', calcFunc);
+    materialBlock.addEventListener('change', calcFunc);
+    optionsBlock.addEventListener('change', calcFunc);
+    promocodeBlock.addEventListener('input', calcFunc);
 };
 
 export default calc;
