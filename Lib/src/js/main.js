835960f868909e08f090824ab3c14f1ef64844b0
Dmitry Ageev <dimichageev@gmail.com>
2020-10-29 13:32:43 +0300

    042 end

--- Lib/src/js/main.js
+++ Lib/src/js/main.js
@@ -19,3 +19,16 @@ import $ from './lib/lib';
 //  $('button').fadeIn(1800);
 
 // console.log($('button').html('Hello'));
+
+
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
+});
+
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
+});
+
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
