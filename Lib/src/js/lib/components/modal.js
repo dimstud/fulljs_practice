0e37bd294659b9f486604c667bf3a321e5b6de76
Dmitry Ageev <dimichageev@gmail.com>
2020-11-01 13:37:55 +0300

    046 4:44m

--- Lib/src/js/lib/components/modal.js
+++ Lib/src/js/lib/components/modal.js
@@ -26,3 +26,33 @@ $.prototype.modal = function () {
     });
 };
 $('[data-toggle="modal"]').modal();
+// Создание модального окна (объект настроек{ титульник и боди тело, кол. кнопок и настройки для кнопок})
+$.prototype.createModal = function({text, btns} = {}) {
+    for (let i = 0; i < this.length; i++) {
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+
+        modal.innerHTML = `
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <button class="close" data-close>
+                    <span>&times;</span>
+                </button>
+                <div class="modal-header">
+                    <div class="modal-title">
+                        Modal title #2
+                    </div>
+                </div>
+                <div class="modal-body">
+                    Lorem ipsum dolor sit
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-danger" data-close>Close</button>
+                    <button class="btn btn-success">Save changes</button>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+};
